dd31faad45f28c484cde190ebc6c3a53a5ac1c0f
tbalint02 <balint.takacs02@gmail.com>
2024-10-09 10:52:56 +0200

    kommentezett

--- main (1).cpp
+++ main (1).cpp
@@ -4,23 +4,28 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[NELEMENTS];  // Rosszul volt megadva a név
+
     std::cout << '1-100 ertekek duplazasa'
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //for ciklus kiegészítése
+
     {
         std::cout << "Ertek:"
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    std::cout << "Atlag szamitasa: " << std::endl; //Hiányzo zárás és értékek kimutatása
+
+    int atlag; // Inicializálás
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
+    std::cout << "Atlag: " << atlag << std::endl; //átlag kiírása
+    //mamoria felszabaditas
+
     return 0;
 }
